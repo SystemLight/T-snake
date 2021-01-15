66101136bf68603479e63e6459ba9342a50e4932
SystemLight <1466335092@qq.com>
2021-01-15 19:20:20 +0800

    [2021-01-15]初始化仓库

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # T-snake
+
 Basic development template of Python project
+
+## 安装
+
+```
+
+```
+
+## 用法
+
+用法描述
+
+## License
+
+T-snake uses the MIT license, see LICENSE file for the details.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name="snake",
+    python_requires=">=3.5",
+    version="1.0.0",
+    author="SystemLight",
+    author_email="1466335092@qq.com",
+    maintainer="SystemLight",
+    maintainer_email="1466335092@qq.com",
+    url="",
+    license="MIT",
+    description="",
+    long_description="",
+    long_description_content_type='text/markdown',
+    download_url="",
+    install_requires="",
+    platforms=["Windows", "Linux"],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    py_modules=[],
+    scripts=[],
+    entry_points={}
+)
